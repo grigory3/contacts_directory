9661f392d9964a8d88f4ba0f8bcc9c3c3a970489
grigory <grigory@contacts-directory.com>
2024-10-10 13:06:32 +0300

    init commit

---
+++ src/main/java/com/example/contacts_directory/ContactsDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.contacts_directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsDirectoryApplication.class, args);
+	}
+
+}
